bfc77673c0a538a013b2002fa75f79bee86cf2b7
Madhusudan Sharma <madhusudansharma516@gmail.com>
2024-07-24 21:29:00 +0530

    dsd

--- js/numberGame.js
+++ js/numberGame.js
@@ -15,3 +15,41 @@ function RandomNumber() {
   return randomNumber;
 }
 
+
+const startBtn = document.getElementById('startButton');
+const userChoice = document.querySelector("#userNum");
+
+
+
+document.getElementById('startButton').addEventListener('click', () => {
+  const boxes = document.querySelectorAll('.numberBox');
+  let currentIndex = 0;
+  let blinkCount = 0;
+  const maxBlinks = 5 + Math.floor(Math.random() * 2); // 6 to 7 times
+  startBtn.disabled = true;
+
+  const blinkInterval = setInterval(() => {
+    boxes.forEach(box => box.classList.remove('blink'));
+    boxes[currentIndex].classList.add('blink');
+    currentIndex = (currentIndex + 1) % boxes.length;
+    blinkCount++;
+
+    if (blinkCount > maxBlinks * boxes.length) {
+      clearInterval(blinkInterval);
+      const randomIndex = Math.floor(Math.random() * boxes.length);
+      boxes.forEach(box => box.classList.remove('blink'));
+      boxes[randomIndex].classList.add('blink');
+      document.getElementById('result').innerText = `Your random number is ${randomIndex + 1}`;
+
+      if (userChoice.value === randomIndex + 1) {
+        console.log('win');
+
+      } else {
+        console.log('lose');
+
+      }
+
+    }
+  }, 300);
+});
+
